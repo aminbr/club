c17abc5d007dc91b48606ac6586f2ee5f9e21e03
asus <asus@DESKTOP-T66413O>
2017-05-26 00:17:26 +0430

    add identity class for model user in module account

--- config/web.php
+++ config/web.php
@@ -20,7 +20,7 @@ $config = [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\modules\account\models\User',
+            'identityClass' => 'app\modules\account\models\User', 
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
