42fd43181a2432ef26d2205aea5384c8acc6092d
asus <asus@DESKTOP-T66413O>
2017-05-25 23:55:52 +0430

    add rbac dbManager

--- config/console.php
+++ config/console.php
@@ -22,6 +22,9 @@ $config = [
         ],
         'db' => $db,
     ],
+    'authManager' => [
+        'class' => 'yii\rbac\DbManager',
+    ],
     'params' => $params,
     /*
     'controllerMap' => [
