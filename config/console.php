ae43a5d9eb5ff04e1ec87292fbc086692532af6d
asus <asus@DESKTOP-T66413O>
2017-05-26 00:13:09 +0430

    add rbac authDb

--- config/console.php
+++ config/console.php
@@ -21,9 +21,9 @@ $config = [
             ],
         ],
         'db' => $db,
-    ],
-    'authManager' => [
-        'class' => 'yii\rbac\DbManager',
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
     ],
     'params' => $params,
     /*
